c2aef07a44a376b6553ad6887b7c4b9ebd30843f
Alrik He <alrik_he@hotmail.com>
2025-11-26 10:22:46 +0100

    cinemachine cam

--- Assets/FPS/AlriksFPSController.cs
+++ Assets/FPS/AlriksFPSController.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Cinemachine;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(CharacterController))]
 public class AlriksFPSController : MonoBehaviour
@@ -31,22 +33,68 @@ public class AlriksFPSController : MonoBehaviour
     float rotationX = 0;
 
     public bool canMove = true;
-    public bool isRunning, isScoped;
+    public bool isRunning, isScoped,isCinemachine;
+
 
     CharacterController characterController;
+   // public CinemachineVirtualCamera vcam;  // gammal
+    public CinemachineCamera ccam;
+
+    /*void Start()
+    {
+        if (vCam == null)
+            vCam = GetComponent<CinemachineCamera>();
+
+        var lens = vCam.m_Lens;
+        lens.FieldOfView = normalFOV;
+        vCam.m_Lens = lens;
+    }
+
+    void Update()
+    {
+        bool isSprinting = Keyboard.current.leftShiftKey.isPressed; // byt till din egen flagga vid behov
+
+        float targetFov = isSprinting ? sprintFOV : normalFOV;
+
+        var lens = vCam.m_Lens;
+        lens.FieldOfView = Mathf.Lerp(
+            lens.FieldOfView,
+            targetFov,
+            Time.deltaTime * fovLerpSpeed
+        );
+        vCam.m_Lens = lens;
+    }*/
     void Start()
     {
         characterController = GetComponent<CharacterController>();
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
 
-
+    
         if (cam == null)
             cam = GameObject.FindAnyObjectByType<Camera>();
 
-        currentTargetFOV = normalFOV;
-        cam.fieldOfView = normalFOV;
-        
+  
+        isCinemachine = GameObject.FindAnyObjectByType<CinemachineCamera>();
+        print(isCinemachine + " it EXISTS , we have cinemachine");
+        if (isCinemachine)
+        {
+            ccam = GameObject.FindAnyObjectByType<CinemachineCamera>().GetComponent<CinemachineCamera>();
+            currentTargetFOV = normalFOV;
+            // var lens = ccam.Lens;
+            // lens.FieldOfView = normalFOV;
+            // ccam.Lens = lens;
+
+            ccam.Lens.FieldOfView = normalFOV;
+
+
+        }
+        else
+        {
+            playerCamera = GameObject.Find("Main Camera").GetComponent<Camera>();
+            currentTargetFOV = normalFOV;
+            cam.fieldOfView = normalFOV;
+        }
     }
 
     void Update()
@@ -56,22 +104,28 @@ public class AlriksFPSController : MonoBehaviour
         Vector3 forward = transform.TransformDirection(Vector3.forward);
         Vector3 right = transform.TransformDirection(Vector3.right);
 
+        Input.GetKey(KeyCode.Escape);
 
         if (Input.GetKeyDown(KeyCode.LeftShift) || Input.GetKeyUp(KeyCode.LeftShift))
         {
             // Om den onpress ner eller inte onpress ner
-            isRunning = Input.GetKeyDown(KeyCode.LeftShift) | !Input.GetKeyUp(KeyCode.LeftShift);
+            isRunning = Input.GetKeyDown(KeyCode.LeftShift) || !Input.GetKeyUp(KeyCode.LeftShift);
             currentTargetFOV = isRunning ? sprintFOV : normalFOV;
         }
 
-
+        //scope
         if (Input.GetMouseButtonDown(1) || Input.GetMouseButtonUp(1))
         {
             // Om den onpress ner eller inte onpress ner
             isScoped = Input.GetMouseButtonDown(1) | !Input.GetMouseButtonUp(1);
             lookSpeed = isScoped ? 0.1f : 2;
             currentTargetFOV = isScoped ? scopeFOV : normalFOV;
-            cam.fieldOfView = currentTargetFOV;
+
+            if (isCinemachine)
+                cam.fieldOfView = currentTargetFOV;
+           else
+                cam.fieldOfView = currentTargetFOV;
+           
         }
 
 
@@ -84,18 +138,14 @@ public class AlriksFPSController : MonoBehaviour
 
         #region Jumping
         if (Input.GetButton("Jump") && canMove && characterController.isGrounded)
-        {
             moveDirection.y = jumpPower;
-        }
         else
-        {
             moveDirection.y = movementDirectionY;
-        }
+        
 
         if (!characterController.isGrounded)
-        {
             moveDirection.y -= gravity * Time.deltaTime;
-        }
+
 
         #endregion
 
@@ -106,19 +156,23 @@ public class AlriksFPSController : MonoBehaviour
         {
             rotationX += -Input.GetAxis("Mouse Y") * lookSpeed;
             rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
-            playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+            if (!isCinemachine)
+                playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
             transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
           
 
         }
         #endregion
 
-        #region Sprint
+   
   
   
-       
-        cam.fieldOfView = Mathf.Lerp( cam.fieldOfView, currentTargetFOV,  Time.deltaTime * fovLerpSpeed);
-        #endregion
+       //if(cam.fieldOfView >= currentTargetFOV - 0.01f && cam.fieldOfView <= currentTargetFOV + 0.01f)
+       if(isCinemachine)
+            ccam.Lens.FieldOfView = Mathf.Lerp(ccam.Lens.FieldOfView, currentTargetFOV, Time.deltaTime * fovLerpSpeed);
+        else
+            cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, currentTargetFOV, Time.deltaTime * fovLerpSpeed);
+      
 
 
     }
